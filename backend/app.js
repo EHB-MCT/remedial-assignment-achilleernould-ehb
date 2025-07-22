0dcf6a096b1b67d1c0fc8366b52fa6d77b91a216
achilleernould-ehb <114060932+achilleernould-ehb@users.noreply.github.com>
2025-07-22 13:41:26 +0200

    feat: add working backend with Express and MongoDB connection

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const playerRoutes = require("./routes/playerRoute");
+app.use("/api", playerRoutes);
+
 mongoose
   .connect(
-    "mongodb+srv://achilleernould:fj6RdTccyGEd8XIA@web2.bggqmql.mongodb.net/?retryWrites=true&w=majority&appName=web2"
+    "mongodb+srv://achilleernould:Achille123@web2.bggqmql.mongodb.net/economy-simulator?retryWrites=true&w=majority&appName=web2"
   )
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
---
+++ backend/models/player.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const playerSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    money: { type: Number, default: 1000 },
+    resources: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("player", playerSchema);
